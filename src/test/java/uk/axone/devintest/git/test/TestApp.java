97fc908ab637de4165b4f0d12db2ec8c62706603
ramduvvuri <ram.duvvuri@gmail.com>
2023-05-21 08:15:10 +0100

    Jira 2367 - Added new unit tests

--- src/test/java/uk/axone/devintest/git/test/TestApp.java
+++ src/test/java/uk/axone/devintest/git/test/TestApp.java
@@ -9,6 +9,16 @@ public class TestApp {
         System.out.println("Running test 1");
     }
 
+    @Test
+    public void test2(){
+        System.out.println("Running test 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Running test 3");
+    }
+
     private void doSomething(){
         System.out.println("doing something");
     }
